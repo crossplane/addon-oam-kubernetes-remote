0c91d97a4eec1643a0063a565cc420c8c0b86e46
hasheddan <georgedanielmangum@gmail.com>
2020-03-20 10:17:20 -0500

    Use crossplane-runtime for shared reconcilers and crossplane for translators
    Signed-off-by: hasheddan <georgedanielmangum@gmail.com>

--- pkg/controller/containerizedworkload/manualscaler.go
+++ pkg/controller/containerizedworkload/manualscaler.go
@@ -17,25 +17,18 @@ limitations under the License.
 package containerizedworkload
 
 import (
-	"context"
 	"strings"
 
-	"github.com/pkg/errors"
-	appsv1 "k8s.io/api/apps/v1"
-	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 
 	"github.com/crossplane/crossplane-runtime/pkg/event"
 	"github.com/crossplane/crossplane-runtime/pkg/logging"
+	"github.com/crossplane/crossplane-runtime/pkg/reconciler/oam/trait"
+	"github.com/crossplane/crossplane-runtime/pkg/resource"
 	oamv1alpha2 "github.com/crossplane/crossplane/apis/oam/v1alpha2"
 	workloadv1alpha1 "github.com/crossplane/crossplane/apis/workload/v1alpha1"
-
-	"github.com/crossplane/addon-oam-kubernetes-remote/pkg/reconciler/trait"
-)
-
-const (
-	errNotDeployment        = "object to be modified is not a deployment"
-	errNotManualScalerTrait = "trait is not a manual scaler"
+	client "github.com/crossplane/crossplane/pkg/oam/trait"
+	"github.com/crossplane/crossplane/pkg/oam/trait/manualscaler"
 )
 
 // SetupManualScalerTrait adds a controller that reconciles ManualScalers that
@@ -47,25 +40,10 @@ func SetupManualScalerTrait(mgr ctrl.Manager, l logging.Logger) error {
 		Named(name).
 		For(&oamv1alpha2.ManualScalerTrait{}).
 		Complete(trait.NewReconciler(mgr,
-			trait.Kind(oamv1alpha2.ManualScalerTraitGroupVersionKind),
-			trait.Kind(workloadv1alpha1.KubernetesApplicationGroupVersionKind),
+			resource.TraitKind(oamv1alpha2.ManualScalerTraitGroupVersionKind),
+			resource.ObjectKind(workloadv1alpha1.KubernetesApplicationGroupVersionKind),
 			trait.WithLogger(l.WithValues("controller", name)),
 			trait.WithRecorder(event.NewAPIRecorder(mgr.GetEventRecorderFor(name))),
-			trait.WithModifier(trait.NewWorkloadModifierWithAccessor(manualScalerModifier, trait.DeploymentFromKubeAppAccessor)),
+			trait.WithModifier(trait.NewWorkloadModifierWithAccessor(manualscaler.DeploymentModifier, client.DeploymentFromKubeAppAccessor)),
 		))
-}
-
-func manualScalerModifier(ctx context.Context, obj runtime.Object, t trait.Trait) error {
-	d, ok := obj.(*appsv1.Deployment)
-	if !ok {
-		return errors.New(errNotDeployment)
-	}
-
-	ms, ok := t.(*oamv1alpha2.ManualScalerTrait)
-	if !ok {
-		return errors.New(errNotManualScalerTrait)
-	}
-	d.Spec.Replicas = &ms.Spec.ReplicaCount
-
-	return nil
 }
